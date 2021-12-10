b225f11216cbce817e0a20b2c483875e51a557b2
petergridge <40281772+petergridge@users.noreply.github.com>
2021-12-10 14:54:17 +1100

    Refactor to process data based on last 24hr

--- openweathermaphistory/sensor.py
+++ openweathermaphistory/sensor.py
@@ -7,7 +7,7 @@ import voluptuous as vol
 import json
 import math
 from datetime import datetime, timedelta, timezone
-#from . import create_rest_data_from_config , create_weatherhist
+
 from homeassistant.components.sensor import (
     PLATFORM_SCHEMA,
     SensorEntity,
@@ -35,8 +35,6 @@ from .const import (
     ATTR_3_MIN,
     ATTR_4_MAX,
     ATTR_4_MIN,
-    ATTR_5_MAX,
-    ATTR_5_MIN,
     DFLT_ICON_FINE,
     DFLT_ICON_LIGHTRAIN,
     DFLT_ICON_RAIN,
@@ -55,7 +53,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Optional(CONF_LATITUDE): cv.latitude,
         vol.Optional(CONF_LONGITUDE): cv.longitude,
         vol.Optional(CONF_UNIT_SYSTEM, default=CONF_UNIT_SYSTEM_METRIC): cv.unit_system,
-        vol.Optional(ATTR_DAYS, default=5):vol.All(vol.Coerce(int), vol.Range(min=0, max=5)),
+        vol.Optional(ATTR_DAYS, default=4):vol.All(vol.Coerce(int), vol.Range(min=0, max=4)),
         vol.Optional(ATTR_0_MIN, default=1): cv.positive_int,
         vol.Optional(ATTR_0_MAX, default=5): cv.positive_int,
         vol.Optional(ATTR_1_MIN, default=6): cv.positive_int,
@@ -66,8 +64,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Optional(ATTR_3_MAX, default=20): cv.positive_int,
         vol.Optional(ATTR_4_MIN, default=21): cv.positive_int,
         vol.Optional(ATTR_4_MAX, default=25): cv.positive_int,
-        vol.Optional(ATTR_5_MIN, default=26): cv.positive_int,
-        vol.Optional(ATTR_5_MAX, default=30): cv.positive_int,
         vol.Optional(ATTR_ICON_FINE, default=DFLT_ICON_FINE): cv.icon,
         vol.Optional(ATTR_ICON_LIGHTRAIN, default=DFLT_ICON_LIGHTRAIN): cv.icon,
         vol.Optional(ATTR_ICON_RAIN, default=DFLT_ICON_RAIN): cv.icon,
@@ -94,11 +90,8 @@ async def _async_create_entities(hass, config, weather):
     day3min   = config[ATTR_3_MIN]
     day4max   = config[ATTR_4_MAX]
     day4min   = config[ATTR_4_MIN]
-    day5max   = config[ATTR_5_MAX]
-    day5min   = config[ATTR_5_MIN]
-
-    daymax = [day0max,day1max,day2max,day3max,day4max,day5max]
-    daymin = [day0min,day1min,day2min,day3min,day4min,day5min]
+    daymax = [day0max,day1max,day2max,day3max,day4max]
+    daymin = [day0min,day1min,day2min,day3min,day4min]
 
     sensors.append(
         RainFactor(
@@ -128,7 +121,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
         lat = hass.config.latitude
         lon = hass.config.longitude
 
-    for n in range(days + 1):
+    for n in range(days + 2):
         rest = RestData()
         dt = int((datetime.now(tz=timezone.utc)- timedelta(days=n)).timestamp())
         url = CONST_API_CALL % (lat, lon, dt, key, units)
@@ -228,24 +221,24 @@ class RainFactor(SensorEntity):
 
     async def async_update(self):
         #first time today reload the weather for all days
-        if self._ran_today != datetime.utcnow().date().strftime('%Y-%m-%d'):
-            #reload all weather on a new day
-            self._ran_today = datetime.utcnow().date().strftime('%Y-%m-%d')
-            n = 0
-            for rest in self._weather:
-                dt = int((datetime.now(tz=timezone.utc)- timedelta(days=n)).timestamp())
-                url = CONST_API_CALL % (self._lat,self._lon,dt,self._key,self._units)
-                await rest.set_resource(self._hass,url)
-                await rest.async_update(log_errors=False)
-                n += 1
-            _LOGGER.debug('new day update has run successfully')
-        else:
+#        if self._ran_today != datetime.utcnow().date().strftime('%Y-%m-%d'):
+        #reload all weather on a new day
+#        self._ran_today = datetime.utcnow().date().strftime('%Y-%m-%d')
+        n = 0
+        for rest in self._weather:
+            dt = int((datetime.now(tz=timezone.utc)- timedelta(days=n)).timestamp())
+            url = CONST_API_CALL % (self._lat,self._lon,dt,self._key,self._units)
+            await rest.set_resource(self._hass,url)
+            await rest.async_update(log_errors=False)
+            n += 1
+        _LOGGER.debug('new day update has run successfully')
+ #       else:
             #only reload today's weather
-            _LOGGER.debug("weather 0 before")
-            _LOGGER.debug(self._weather[0].data)
-            await self._weather[0].async_update(log_errors=False)
-            _LOGGER.debug("weather 0 after")
-            _LOGGER.debug(self._weather[0].data)
+#            _LOGGER.debug("weather 0 before")
+#            _LOGGER.debug(self._weather[0].data)
+#            await self._weather[0].async_update(log_errors=False)
+#            _LOGGER.debug("weather 0 after")
+#            _LOGGER.debug(self._weather[0].data)
 
         await self._weatherhist.set_weather(self._weather, self._daymin, self._daymax) 
         await self._weatherhist.async_update()        
--- openweathermaphistory/weatherhistory.py
+++ openweathermaphistory/weatherhistory.py
@@ -3,6 +3,7 @@ import logging
 import json
 import math
 from datetime import datetime, timedelta, timezone
+import pytz
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,56 +28,69 @@ class WeatherHist:
         _LOGGER.debug("updating weatherhistory")
         n = 0
         minfac = 1
+        MINTEMP = {0:999,1:999,2:999,3:999,4:999,5:999}
+        MAXTEMP = {0:-999,1:-999,2:-999,3:-999,4:-999,5:-999}
         ATTRS = {}
-        ATTRSRAIN = {}
-        ATTRSCUM = {}
+        ATTRSRAIN = {'day_0_rain':0,'day_1_rain':0,'day_2_rain':0,'day_3_rain':0,'day_4_rain':0,'day_5_rain':0}
         ATTRMIN = {}
         ATTRMAX = {}
+        TOTAL = {0:0,1:0,2:0,3:0,4:0,5:0}
         cumulative = 0
         for rest in self._weather:
 
             data = json.loads(rest.data)
-            total = 0
-            mintemp = 999
-            maxtemp = -999
+            localtimezone = pytz.timezone(data["timezone"])
+
+            current = data["current"]
+            if 'dt' in current:
+                dt = current["dt"]
+                formatted_dt = datetime.utcfromtimestamp(dt).replace(tzinfo=timezone.utc).astimezone(tz=None).strftime('%Y-%m-%d %H:%M:%S')
+                ATTRS ["As at"] = formatted_dt
+#                _LOGGER.warning("AS AT %s",formatted_dt)
+
             hourly = data["hourly"]
             for hour in hourly:
-                #the latest time for the current day
-                if n == 0:
-                    if 'dt' in hour:
-                        dt = hour["dt"]
-                        formatted_dt = datetime.utcfromtimestamp(dt).replace(tzinfo=timezone.utc).astimezone(tz=None).strftime('%Y-%m-%d %H:%M:%S')
-                        ATTRS ["As at"] = formatted_dt
-                        _LOGGER.debug("AS AT %s",formatted_dt)
+
+                # now determine the local day the last 24hrs = 0 24-48 = 1...
+                localday = datetime.utcfromtimestamp(hour["dt"]).replace(tzinfo=timezone.utc).astimezone(tz=localtimezone)
+                localnow = datetime.now(localtimezone)
+                localdaynum = (localnow - localday).days
+
+                if localdaynum >= len(self._daymax):
+                    continue
+
                 if 'rain' in hour:
                     rain = hour["rain"]
-                    if math.isnan(rain["1h"]):
-                        rain["1h"] = 0
-                    else:
-                        total += rain["1h"]
+                    if not math.isnan(rain["1h"]):
+                        TOTAL[localdaynum] += rain["1h"]
+                        ATTRSRAIN ["day_%d_rain"%(localdaynum)] = round(TOTAL[localdaynum],2)
+
                 if 'temp' in hour:
-                    if hour["temp"] < mintemp:
-                        mintemp = hour["temp"]
-                        ATTRMIN["day_%d_min"%(n)] = mintemp
-                    if hour["temp"] > maxtemp:
-                        maxtemp = hour["temp"]
-                        ATTRMAX["day_%d_max"%(n)] = maxtemp
-                    
+                    if hour["temp"] < MINTEMP[localdaynum]:
+                        MINTEMP[localdaynum] = hour["temp"]
+                        ATTRMIN["day_%d_min"%(localdaynum)] = hour["temp"]
+                    if hour["temp"] > MAXTEMP[localdaynum]:
+                        MAXTEMP[localdaynum] = hour["temp"]
+                        ATTRMAX["day_%d_max"%(localdaynum)] = hour["temp"]
+            
             #end hour loop
 
-            cumulative = cumulative + total
-            ATTRSCUM ["day_%d_cumulative"%(n)] = round(cumulative,2)
-            ATTRSRAIN ["day_%d_rain"%(n)] = round(total,2)
+        #end rest loop
+        
+        #now loop through the data to calculate the adjustment factor
+        for x in range(len(self._daymax)):
+            
+            cumulative += ATTRSRAIN ["day_%d_rain"%(x)]
             try:
-                dayfac = 1 - ( cumulative - self._daymin[n])/(self._daymax[n]-self._daymin[n])
+                dayfac = 1 - ( cumulative - self._daymin[localdaynum])/(self._daymax[localdaynum]-self._daymin[localdaynum])
                 if dayfac < minfac:
                     minfac = dayfac
             except:
                 dayfac = 1
-            n += 1
-        #end rest loop
+        
         if minfac < 0:
             minfac = 0
 
         self.factor = round(minfac,2)
-        self.attrs = {**ATTRS, **ATTRSRAIN, **ATTRMIN, **ATTRMAX}
+#        self.attrs = {**ATTRS, **ATTRSRAIN, **ATTRMIN, **ATTRMAX}
+        self.attrs = {**ATTRSRAIN, **ATTRMIN, **ATTRMAX}
